c5c8657339987c1ff53862dd454f0f7de24e1726
Pavel3444 <burikov.pavel3444@yandex.ru>
2025-10-14 22:27:09 +0300

    add menu

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+    Console.WriteLine("Menu");
+    Console.WriteLine("1. New game");
+    Console.WriteLine("2. Load game");
+    Console.WriteLine("3. Save game");
+    Console.WriteLine("0. Quit");
 
-Console.WriteLine("Hello, World!");
+    string userInput = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(userInput)
+        || !int.TryParse(userInput, out int userChoice)
+        || userChoice < 0 
+        || userChoice > 3
+       )
+    {
+        Console.WriteLine("Wrong! Enter 1,2,3 or 0.");
+        continue;
+    }
+    Console.WriteLine("Wonderful!");
+    break;
+}
+
